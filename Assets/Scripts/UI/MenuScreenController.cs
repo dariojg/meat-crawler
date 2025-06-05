535b9416147a1fb7aa6c407106814952c883a6f5
Dario J. Grinberg <dariogrinberg@gmail.com>
2025-06-05 18:30:49 -0300

    prototipo uificado

--- Assets/Scripts/UI/MenuScreenController.cs
+++ Assets/Scripts/UI/MenuScreenController.cs
@@ -1,5 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -7,8 +5,7 @@ public class MenuScreenController : MonoBehaviour
 {
     public void PlayGame()
     {
-        SceneManager.LoadScene("SampleScene"); // OJO, creo que aca puede dar error asi que cualquier
-                                               // cosa cambia el nombre entre "" si la escena tiene otro name 
+        SceneManager.LoadScene("MainScene"); 
     }
 
     public void QuitGame()
